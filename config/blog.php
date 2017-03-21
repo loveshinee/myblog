ad0d1800348e53f4e059aa0afed1603d23503c3e
张泽雨的大砖块 <zhangzy1203@gmail.com>
2017-03-21 11:31:07 +0800

    finish blog theme

--- config/blog.php
+++ config/blog.php
@@ -1,7 +1,12 @@
 <?php
 
 return [
-        'title' => 'My Blog',
+		'name' => 'Blog By Zhangzy',
+        'title' => 'Blog By Zhangzy',
+        'subtitle' => 'http://www.zhangzy.com.cn',
+        'description' => '这是一个叫老张的人乱写的博客',
+        'author' => 'zhangzy',
+        'page_image' => 'home-bg.jpg',
         'posts_per_page' => 5,
         'uploads' => [
         	'storage' => 'local',
--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -13,15 +13,18 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         Model::unguard();
+        $this->call('TagTableSeeder');
         $this->call('PostTableSeeder');
+
+        Model::reguard();
     }
 }
 
-class PostTableSeeder extends Seeder
-{
-    public function run()
-    {
-        App\Post::truncate();
-        factory(App\Post::class, 20)->create();
-    }
-}
+// class PostTableSeeder extends Seeder
+// {
+//     public function run()
+//     {
+//         App\Post::truncate();
+//         factory(App\Post::class, 20)->create();
+//     }
+// }
